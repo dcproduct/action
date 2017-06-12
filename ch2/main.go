7a05da2667b542f0bdb31560492040f0d50662c3
dcproduct@163.com <dcproduct@163.com>
2017-06-12 14:25:40 +0800

    third commit

--- ch2/main.go
+++ ch2/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"log"
 	"os"
-	_"github.com/dcproduct/action/ch2/matchers"
+
+	_ "github.com/dcproduct/action/ch2/matchers"
 	"github.com/dcproduct/action/ch2/search"
 )
 
-func init()  {
+func init() {
 	log.SetOutput(os.Stdout)
 }
 
-func main()  {
+// Add comment to test.
+func main() {
 	search.Run("president")
 }
